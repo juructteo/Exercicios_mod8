66ae6f24465f4eda14f3029c93d9a68ed8649095
Julia Téo <102557389+juructteo@users.noreply.github.com>
2022-07-06 17:12:41 -0300

    Add files via upload

--- specs/units/services/CpfValidationService.spec.ts
+++ specs/units/services/CpfValidationService.spec.ts
@@ -11,34 +11,35 @@ describe("RandomService", () => {
 
     it('Deveria validar quando inserido um cpf existente', () => {
         // Arrange
-        const new_doc = 22788643825
+        const new_doc = 22788643861
         // Act
         const result = _service.validarCPF(new_doc)
         // Assert
-        expect(result).toThrow()
+        expect(result).not.toThrow()
     })
 
     it('Deveria estourar um erro, quando passado um tamanho incorreto ', () => {
 
-        expect(() => _service.validarCPF(52145826522556)).toThrow('Apenas 11 são digitos são permitidos.')
+        expect(() => { _service.validarCPF(52145826522556) }).toThrow('CPF Invalido. São somente 11 digitos')
 
 
     })
 
     it('Deveria estourar um erro, quando passado um tamanho incorreto ', () => {
 
-        expect(() => _service.validarCPF(5214)).toThrow('São necessários 11  digitos.')
+        expect(() => { _service.validarCPF(5214) }).toThrow('CPF Invalido. São no minimo 11 digitos')
 
 
     })
 
     it('Deveria estourar um erro, quando passado um tipo incorreto NaN', () => {
-        expect(() => _service.validarCPF(Number.NaN)).toThrow()
+        expect(() => { _service.validarCPF(Number.NaN) }).toThrow()
     })
 
-    it('Deveria estourar um erro ao passar um número negativo', () => {
-        expect(() => _service.validarCPF(-25215)).toThrow()
+    it('Se um número negativo for passador, deve falhar', () => {
+        expect(() => { _service.validarCPF(-123123) }).toThrow("CPF não pode ser negativo")
     })
+
 
 
 })
